ad005aa189dfae09333b31dc3953aa917269b987
KenjiAu-Creator <58325531+KenjiAu-Creator@users.noreply.github.com>
2021-02-24 20:42:17 -0700

    Updates

---
+++ precache-manifest.3457fe0d5d65d137e2b02d68d8ab0d54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f67b8965c92c0a2cef8114b9f7dff540",
+    "url": "/React-Redux-Router-Calc/index.html"
+  },
+  {
+    "revision": "7bb6bb8b6ff6582b9c0b",
+    "url": "/React-Redux-Router-Calc/static/css/main.6fff9fad.chunk.css"
+  },
+  {
+    "revision": "651b4e34a51e09c17974",
+    "url": "/React-Redux-Router-Calc/static/js/2.6c7fe1e6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Redux-Router-Calc/static/js/2.6c7fe1e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bb6bb8b6ff6582b9c0b",
+    "url": "/React-Redux-Router-Calc/static/js/main.b1a685ca.chunk.js"
+  },
+  {
+    "revision": "cacc6cad7c7075dba969",
+    "url": "/React-Redux-Router-Calc/static/js/runtime-main.f0101fea.js"
+  }
+]);
